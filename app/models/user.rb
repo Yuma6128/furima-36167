da2bc950e6844f100c22682eb1f1568ef4ed75ad
Yuma6128 <87649468+Yuma6128@users.noreply.github.com>
2021-09-04 20:12:25 +0900

    バリデーションのスペル直し

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validate :nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday,presence: true
+         validates :nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday,presence: true
 end
