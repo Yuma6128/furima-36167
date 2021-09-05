e9efbe36582891c2ab0d44608c1913b8a2a1a148
Yuma6128 <87649468+Yuma6128@users.noreply.github.com>
2021-09-05 14:40:54 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday,presence: true
+  validates :nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday,presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :last_name_kana,:first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
 end
