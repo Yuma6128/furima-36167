f254584a8c686c3c467d8659799fcaca819aea3a
Yuma6128 <87649468+Yuma6128@users.noreply.github.com>
2021-09-07 07:29:31 +0900

    名前(全角)バリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday,presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :last_name_kana,:first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name,:first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
 end
