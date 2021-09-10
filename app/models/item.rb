b7d5950f79ab5cefe7f0d2c4f29e7edfd076ff91
Yuma6128 <87649468+Yuma6128@users.noreply.github.com>
2021-09-11 08:30:05 +0900

    Activeハッシュの設定
    カテゴリー
    商品の状態
    配送料
    発送元
    発送までの日数

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shipping_fee_status
+  belongs_to :item_prefecture
+  belongs_to :item_scheduled_delivery
+  validates :item_category_id,:item_sales_status_id,:item_shipping_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
