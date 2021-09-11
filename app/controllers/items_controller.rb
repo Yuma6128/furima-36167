4e84253d1340567114224be55545a8f68d344b2c
Yuma6128 <87649468+Yuma6128@users.noreply.github.com>
2021-09-11 15:42:24 +0900

    商品を出品する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+       redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
+
   def item_params
-    params.require(:item).permit(:image,:item_name,:item_explain,:item_category_id,:item_sales_status_id,:item_shipping_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id,:item_price,:user)
+    params.require(:item).permit(:image,:item_name,:item_explain,:item_category_id,:item_sales_status_id,:item_shipping_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id,:item_price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  validates :item_name,:item_explain,:item_price, presence: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_category
   belongs_to :item_sales_status
@@ -8,5 +9,4 @@ class Item < ApplicationRecord
   belongs_to :item_prefecture
   belongs_to :item_scheduled_delivery
   validates :item_category_id,:item_sales_status_id,:item_shipping_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
-
 end
