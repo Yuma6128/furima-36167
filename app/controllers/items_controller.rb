e7601e27a42262aa582608e91c72ae6d3ddebb05
Yuma6128 <87649468+Yuma6128@users.noreply.github.com>
2021-09-11 11:07:51 +0900

    itemsテーブルに画像ファイル紐付け

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:item_name,:item_explain,:item_category_id,:item_sales_status_id,:item_shipping_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id,:item_price,:user)
+    params.require(:item).permit(:image,:item_name,:item_explain,:item_category_id,:item_sales_status_id,:item_shipping_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id,:item_price,:user)
   end
 end
