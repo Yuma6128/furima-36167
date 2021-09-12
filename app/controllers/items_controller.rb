501f94b6c33c5dc8bdf86142191aa1f8d75c6cbf
Yuma6128 <87649468+Yuma6128@users.noreply.github.com>
2021-09-12 12:00:23 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,15 +11,16 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-       redirect_to root_path
+      redirect_to root_path
     else
       render 'new'
-    end 
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image,:item_name,:item_explain,:item_category_id,:item_sales_status_id,:item_shipping_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id,:item_price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_explain, :item_category_id, :item_sales_status_id,
+                                 :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,6 +12,61 @@ RSpec.describe Item, type: :model do
       end
     end
     context '商品登録できないとき' do
+      it '画像がないと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名がないと登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it '商品詳細がないと登録できない' do
+        @item.item_explain = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item explain can't be blank")
+      end
+      it '商品カテゴリーが初期値だと登録できない' do
+        @item.item_category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item category can't be blank")
+      end
+      it '商品状態が初期値だと登録できない' do
+        @item.item_sales_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item sales status can't be blank")
+      end
+      it '配送料負担が初期値だと登録できない' do
+        @item.item_shipping_fee_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item shipping fee status can't be blank")
+      end
+      it '発送元の地域が初期値だと登録できない' do
+        @item.item_prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item prefecture can't be blank")
+      end
+      it '発送までの日数が初期値だと登録できない' do
+        @item.item_scheduled_delivery_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item scheduled delivery can't be blank")
+      end
+      it '価格が300円より少ないと登録できない' do
+        @item.item_price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price must be greater than or equal to 300')
+      end
+      it '価格が9,999,999円より大きいと登録できない' do
+        @item.item_price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price must be less than or equal to 9999999')
+      end
+      it '価格は半角でないと登録できない' do
+        @item.item_price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price is not a number')
       end
     end
   end
+end
