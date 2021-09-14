736dd17d86407724eb3222d0a54d92d84b8ec6c1
Yuma6128 <87649468+Yuma6128@users.noreply.github.com>
2021-09-15 08:06:01 +0900

    出品者以外の削除でTOPへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit ,:destroy]
   before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
@@ -36,8 +36,12 @@ class ItemsController < ApplicationController
   end
 def destroy
    item = Item.find(params[:id])
+   if item.user == current_user
    item.destroy
    redirect_to root_path
+   else
+    redirect_to root_path
+   end
 end
   private
 
