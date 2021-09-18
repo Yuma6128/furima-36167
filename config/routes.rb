b66f88ee0d981fb0980c83bb19c7e32d3a166d33
Yuma6128 <87649468+Yuma6128@users.noreply.github.com>
2021-09-18 12:16:27 +0900

    ルーティングのネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items
-  resources :order
+  resources :orders do
+    resources :addresses,only: :index,:create
+  end
 end
